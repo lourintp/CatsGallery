37c9fecd14fbee56df4ca29c74b21524640db173
lourintp <thiago.lourin@objective.com.br>
2020-03-11 17:18:15 -0300

    Initial commit

---
+++ CatsGallery/CatsGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatsGallery
+//
+//  Created by Thiago Lourin on 11/03/20.
+//  Copyright © 2020 Lourin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
